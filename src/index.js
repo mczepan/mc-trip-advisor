3286421e7d986eb03b3d01b688fa8d1c1610703b
Maciej Czepan <maciej.czepan@gmail.com>
2023-01-12 12:17:42 +0100

    eslint & prettier & husky

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from 'views/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
